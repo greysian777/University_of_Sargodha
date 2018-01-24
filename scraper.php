91d5ee4208b615004b0239027de78a78783785f8
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-01-24 11:26:05 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -6,8 +6,15 @@ require 'scraperwiki/simple_html_dom.php';
 for($loop = 0; $loop <= 15; $loop+=15)
 {
  $LINK =  file_get_html('https://www.uos.edu.pk/About/alumni_directory/'.$loop);
-  echo $LINK;
-}
+  if ($LINK)
+   {
+			//	Paginate all 'View' buttons
+			foreach ($LINK->find("//*[@id='main_content']/div/div[1]/table") as $element) 
+    {
+     echo  $element; 
+    }
+    
+  }
 ?>
 
 
