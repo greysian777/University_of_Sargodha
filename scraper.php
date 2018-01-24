97740307289e792ecbe4f5f48003f85bd24aa2c3
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-01-24 11:08:46 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -5,7 +5,7 @@ require 'scraperwiki/simple_html_dom.php';
 // // Read in a page
 for($loop = 0; $loop <= 15; $loop+=15)
 {
- $LINK =  file_get_html('https://www.uos.edu.pk/About/alumni_directory'.$loop);
+ $LINK =  file_get_html('https://www.uos.edu.pk/About/alumni_directory/'.$loop);
   echo $LINK;
 }
 ?>
