0c1d1e4e0361b041e4d57b3643553a09ed706be5
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-01-24 11:51:17 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -12,11 +12,11 @@ $url = 'https://www.uos.edu.pk/About/alumni_directory/'.$loop;
 			//	Paginate all 'View' buttons
 			foreach ($LINK->find("//*[@id='main_content']/div/div[1]/table/tbody/tr") as $element) 
     {
-     			$rollno	=	$element->find('td[0]', 0);
-			$fname	=	$element->find('td[1]', 0);
-			$lname	=	$element->find('td[2]', 0);
-			$prog	=	$element->find('td[3]', 0);
-			$passy	=	$element->find('td[4]', 0);
+     			$rollno	=	$element->find('td[0]', 0)->plaintext;
+			$fname	=	$element->find('td[1]', 0)->plaintext;
+			$lname	=	$element->find('td[2]', 0)->plaintext;
+			$prog	=	$element->find('td[3]', 0)->plaintext;
+			$passy	=	$element->find('td[4]', 0)->plaintext;
 				
 				
 				
