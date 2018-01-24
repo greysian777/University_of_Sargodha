4ec08ca2aa782574cc4928c603a8befbbb8f5270
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-01-24 11:50:52 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -5,14 +5,32 @@ require 'scraperwiki/simple_html_dom.php';
 // // Read in a page
 for($loop = 0; $loop <= 15; $loop+=15)
 {
- $LINK =  file_get_html('https://www.uos.edu.pk/About/alumni_directory/'.$loop);
+$url = 'https://www.uos.edu.pk/About/alumni_directory/'.$loop;
+ $LINK =  file_get_html($url);
   if ($LINK)
    {
 			//	Paginate all 'View' buttons
 			foreach ($LINK->find("//*[@id='main_content']/div/div[1]/table/tbody/tr") as $element) 
     {
      			$rollno	=	$element->find('td[0]', 0);
-				echo "$rollno\n";
+			$fname	=	$element->find('td[1]', 0);
+			$lname	=	$element->find('td[2]', 0);
+			$prog	=	$element->find('td[3]', 0);
+			$passy	=	$element->find('td[4]', 0);
+				
+				
+				
+				 $record = array( 'rollno' =>$rollno, 
+		   'fname' => $fname,
+		   'lname' => $lname, 
+		   'prog' => $prog, 
+		   'passy' => $passy, 
+		   'url' => $url);
+						
+						
+           scraperwiki::save(array('fname','lname','prog','passy','url'), $record);
+				
+				
     }
     
   }
